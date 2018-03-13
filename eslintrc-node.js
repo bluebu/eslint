50d85149cb86f0ab0343d3d37c1e6c29efd41b24
bluebu <bluebuwang@gmail.com>
2018-03-13 11:10:47 +0800

    add eslint file for node env

---
+++ eslintrc-node.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+    },
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "globals": {
+        __: 0,
+    },
+    "rules": {
+        "max-len": [
+            "error",
+            { "code": 160 }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-multi-assign": [0],
+        "no-console": "off",
+        "no-underscore-dangle": [
+            "error",
+            { "arrow": [ "__" ] }
+        ],
+    }
+};
